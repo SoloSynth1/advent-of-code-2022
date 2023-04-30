79a6c449f9d82233b766fa252fb49c4ec8b784c9
SoloSynth1 <solosynth1@gmail.com>
2023-04-30 01:52:19 -0700

    finish part 2 of day-5

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let lines = read_lines(file_path);
 
     let mut a = false;
-    let mut crates: Vec<VecDeque<char>> = vec![VecDeque::new(); 9];
+    let mut crates_p1: Vec<VecDeque<char>> = vec![VecDeque::new(); 9];
+    let mut crates_p2: Vec<VecDeque<char>> = vec![VecDeque::new(); 9];
 
     for line in lines {
         let Ok(ip) = line else {panic!("Failed to read line, panic!")};
@@ -19,22 +20,32 @@ fn main() {
             let num_crates: usize = parsed_line[1].parse::<usize>().unwrap();
             let from_crate: usize = parsed_line[3].parse::<usize>().unwrap() - 1;
             let to_crate: usize = parsed_line[5].parse::<usize>().unwrap() - 1;
+            let mut buffer_crates: VecDeque<char> = VecDeque::new();
             for _ in 0..num_crates {
-                let popped_value = crates[from_crate].pop_front().unwrap();
-                crates[to_crate].push_front(popped_value);
+                let popped_value_p1 = crates_p1[from_crate].pop_front().unwrap();
+                let popped_value_p2 = crates_p2[from_crate].pop_front().unwrap();
+                crates_p1[to_crate].push_front(popped_value_p1);
+                buffer_crates.push_front(popped_value_p2);
+            }
+            while buffer_crates.len() > 0 {
+                let popped_value = buffer_crates.pop_front().unwrap();
+                crates_p2[to_crate].push_front(popped_value);
             }
         } else {
             for x in 0..9 {
                 let idx = 1 + ( 4 * x );
                 let sel_char = ip.chars().nth(idx).unwrap();
                 if char::is_alphabetic(sel_char) {
-                    crates[x].push_back(sel_char);
+                    crates_p1[x].push_back(sel_char);
+                    crates_p2[x].push_back(sel_char);
                 }
             }
         }
     }
-    let first_letters = crates.into_iter().map(|col| col[0]).collect::<Vec<char>>();
-    println!("part 1: {}", first_letters.iter().collect::<String>());
+    let first_letters_p1 = crates_p1.into_iter().map(|col| col[0]).collect::<Vec<char>>();
+    println!("part 1: {}", first_letters_p1.iter().collect::<String>());
+    let first_letters_p2 = crates_p2.into_iter().map(|col| col[0]).collect::<Vec<char>>();
+    println!("part 2: {}", first_letters_p2.iter().collect::<String>());
 }
 
 
